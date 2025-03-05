16fac574bd9f6a99fce5d112d95e1cc631fc6cdb
Tshering Phuntsho <0223031.cst@rub.edu.bt>
2025-03-05 08:17:17 +0600

    add navigation

---
+++ Practicals/Practical_1/Gojek/app/tabs/index.tsx
@@ -0,0 +1,31 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
+      <Link href="/tabs/about" style={styles.button}>
+        Go to About screen
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
